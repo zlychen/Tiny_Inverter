42816594019ea5a3e9068414541d0315dd9bc83d
whutzf2010 <whutzf2010@163.com>
2023-11-08 10:09:52 +0800

    Firmware

---
+++ Firmware/STM32F302_General_Model_v1_0/MDK-ARM/TINY_AC_SOURCE_F302/stm32f3xx_ll_usart.d
@@ -0,0 +1,13 @@
+tiny_ac_source_f302\stm32f3xx_ll_usart.o: ../Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_ll_usart.c
+tiny_ac_source_f302\stm32f3xx_ll_usart.o: ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_ll_usart.h
+tiny_ac_source_f302\stm32f3xx_ll_usart.o: ../Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h
+tiny_ac_source_f302\stm32f3xx_ll_usart.o: ../Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f302xc.h
+tiny_ac_source_f302\stm32f3xx_ll_usart.o: ../Drivers/CMSIS/Include/core_cm4.h
+tiny_ac_source_f302\stm32f3xx_ll_usart.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+tiny_ac_source_f302\stm32f3xx_ll_usart.o: ../Drivers/CMSIS/Include/cmsis_version.h
+tiny_ac_source_f302\stm32f3xx_ll_usart.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
+tiny_ac_source_f302\stm32f3xx_ll_usart.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
+tiny_ac_source_f302\stm32f3xx_ll_usart.o: ../Drivers/CMSIS/Include/mpu_armv7.h
+tiny_ac_source_f302\stm32f3xx_ll_usart.o: ../Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h
+tiny_ac_source_f302\stm32f3xx_ll_usart.o: ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_ll_rcc.h
+tiny_ac_source_f302\stm32f3xx_ll_usart.o: ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_ll_bus.h
---
+++ Firmware/STM32F302_General_Model_v1_0/MDK-ARM/TINY_AC_SOURCE_F302/system_stm32f3xx.d
@@ -0,0 +1,10 @@
+tiny_ac_source_f302\system_stm32f3xx.o: ../Core/Src/system_stm32f3xx.c
+tiny_ac_source_f302\system_stm32f3xx.o: ../Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h
+tiny_ac_source_f302\system_stm32f3xx.o: ../Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f302xc.h
+tiny_ac_source_f302\system_stm32f3xx.o: ../Drivers/CMSIS/Include/core_cm4.h
+tiny_ac_source_f302\system_stm32f3xx.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+tiny_ac_source_f302\system_stm32f3xx.o: ../Drivers/CMSIS/Include/cmsis_version.h
+tiny_ac_source_f302\system_stm32f3xx.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
+tiny_ac_source_f302\system_stm32f3xx.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
+tiny_ac_source_f302\system_stm32f3xx.o: ../Drivers/CMSIS/Include/mpu_armv7.h
+tiny_ac_source_f302\system_stm32f3xx.o: ../Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h
